68d8f58e4b974b360e0897d4d9c7ab870cab54f2
Marcos Venicius Ubiali <ubiali.marcosvenicius@gmail.com>
2018-11-29 11:58:45 -0200

    same user info db

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
+const pg = require('pg');
 const uuid = require('uuid');
 
+pg.defaults.ssl = true;
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
@@ -46,6 +48,9 @@ if (!config.EMAIL_TO) {
 }
 if (!config.WEATHER_API_KEY) { //weather api key
 	throw new Error('missing WEATHER_API_KEY');
+}
+if (!config.PG_CONFIG) { //pg config
+	throw new Error('missing PG_CONFIG');
 }
 
 app.set('port', (process.env.PORT || 5000))
@@ -787,10 +792,36 @@ function greetUserText(userId) {
 			var user = JSON.parse(body);
 
 			if (user.first_name) {
-				console.log("FB user: %s %s, %s",
-					user.first_name, user.last_name, user.gender);
+				
+				var pool = new pg.Pool(config.PG_CONFIG);
+				pool.connect(function(err, client, done) {
+					if (err) {
+						return console.error('Error acquiring client', err.stack);
+					}
+					var rows = [];
+					client.query(`SELECT fb_id FROM users WHERE fb_id='${userId}' LIMIT 1`,
+						function(err, result) {
+							if (err) {
+								console.log('Query error: ' + err);
+							} else {
+								if (result.rows.length === 0) {
+									let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic) VALUES ($1, $2, $3, $4)';
+									client.query(sql,
+										[
+											userId,
+											user.first_name,
+											user.last_name,
+											user.profile_pic
+										]);
+								}
+							}
+						});
+					});
+				pool.end();
 
-				sendTextMessage(userId, "Welcome " + user.first_name + '!');
+					sendTextMessage(userId, "Welcome " + user.first_name + '! ' +
+						'I can answer frequently asked questions for you ' +
+						'and I perform job interviews. What can I help you with?');
 			} else {
 				console.log("Cannot get data for fb user with id",
 					userId);
