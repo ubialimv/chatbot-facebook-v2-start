685d0e87b4fcb4eed427970a916b887e2465a2b9
Marcos Venicius Ubiali <ubiali.marcosvenicius@gmail.com>
2018-11-29 14:38:30 -0200

    debug

--- app.js
+++ app.js
@@ -788,34 +788,39 @@ function greetUserText(userId) {
 			console.log('getUserData: ' + user);
 			if (user.first_name) {
 
-                var pool = new pg.Pool(config.PG_CONFIG);
-                pool.connect(function(err, client, done) {
-                    if (err) {
-                        return console.error('Error acquiring client', err.stack);
-                    }
-                    var rows = [];
-                    client.query(`SELECT fb_id FROM users WHERE fb_id='${userId}' LIMIT 1`,
-                        function(err, result) {
-                            if (err) {
-                                console.log('Query error: ' + err);
-                            } else {
-
-                                if (result.rows.length === 0) {
-                                    let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic) ' +
+				var pool = new pg.Pool(config.PG_CONFIG);
+				pool.connect(function(err, client, done) {
+					if (err) {
+						return console.error('Error acquiring client', err.stack);
+					}
+					console.log('connecting on database');
+					var rows = [];
+					console.log('fetching user');
+					client.query(`SELECT id FROM public.users WHERE fb_id='${userId}' LIMIT 1`,
+						function(err, result) {
+							console.log('query result ' + result);
+							if (err) {
+								console.log('Query error: ' + err);
+							} else {
+								console.log('rows: ' + result.rows.length);
+								if (result.rows.length === 0) {
+									let sql = 'INSERT INTO public.users (fb_id, first_name, last_name, profile_picture) ' +
 										'VALUES ($1, $2, $3, $4)';
-                                    client.query(sql,
-                                        [
-                                            userId,
-                                            user.first_name,
-                                            user.last_name,
-                                            user.profile_pic
-                                        ]);
-                                }
-                            }
-                        });
+									console.log('sql: ' + sql);
+									client.query(sql,
+										[
+											userId,
+											user.first_name,
+											user.last_name,
+											user.profile_pic
+										]);
+								}
+							}
+						});
 
-                });
-                pool.end();
+				});
+				pool.end();
+
 
 				sendTextMessage(userId, "Welcome " + user.first_name + '! ' +
                     'I can answer frequently asked questions for you ' +
