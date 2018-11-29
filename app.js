13fae4c77d2d9fb058bb494d4bd54be871dca333
Marcos Venicius Ubiali <ubiali.marcosvenicius@gmail.com>
2018-11-29 14:33:12 -0200

    debug

--- app.js
+++ app.js
@@ -776,7 +776,7 @@ function sendAccountLinking(recipientId) {
 function greetUserText(userId) {
 	//first read user firstname
 	request({
-		uri: 'https://graph.facebook.com/v2.7/' + userId,
+		uri: 'https://graph.facebook.com/v3.2/' + userId,
 		qs: {
 			access_token: config.FB_PAGE_TOKEN
 		}
@@ -785,43 +785,41 @@ function greetUserText(userId) {
 		if (!error && response.statusCode == 200) {
 
 			var user = JSON.parse(body);
-
+			console.log('getUserData: ' + user);
 			if (user.first_name) {
-				
-				var pool = new pg.Pool(config.PG_CONFIG);
-				pool.connect(function(err, client, done) {
-					if (err) {
-						return console.error('Error acquiring client', err.stack);
-					}
-					var rows = [];
-					console.log('fetching user');
-					client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
-						function(err, result) {
-							console.log('query result ' + result);
-							if (err) {
-								console.log('Query error: ' + err);
-							} else {
-								console.log('rows: ' + result.rows.length);
-								if (result.rows.length === 0) {
-									let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_picture) VALUES ($1, $2, $3, $4)';
-									console.log('sql: ' + sql);
-									client.query(sql,
-										[
-											userId,
-											user.first_name,
-											user.last_name,
-											user.profile_pic
-										]);
-								}
-							}
-						});
 
-				});
-				pool.end();
+                var pool = new pg.Pool(config.PG_CONFIG);
+                pool.connect(function(err, client, done) {
+                    if (err) {
+                        return console.error('Error acquiring client', err.stack);
+                    }
+                    var rows = [];
+                    client.query(`SELECT fb_id FROM users WHERE fb_id='${userId}' LIMIT 1`,
+                        function(err, result) {
+                            if (err) {
+                                console.log('Query error: ' + err);
+                            } else {
+
+                                if (result.rows.length === 0) {
+                                    let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic) ' +
+										'VALUES ($1, $2, $3, $4)';
+                                    client.query(sql,
+                                        [
+                                            userId,
+                                            user.first_name,
+                                            user.last_name,
+                                            user.profile_pic
+                                        ]);
+                                }
+                            }
+                        });
+
+                });
+                pool.end();
 
 				sendTextMessage(userId, "Welcome " + user.first_name + '! ' +
-					'I can answer frequently asked questions for you ' +
-					'and I perform job interviews. What can I help you with?');
+                    'I can answer frequently asked questions for you ' +
+                    'and I perform job interviews. What can I help you with?');
 			} else {
 				console.log("Cannot get data for fb user with id",
 					userId);
